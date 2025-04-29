886834414b1c43bc9a321efbf765fb1a0fe8ef3f
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-04-29 21:26:27 +0200

    Spring project starter dowloaded

---
+++ src/main/java/aDarbellay/s05/t1/S05T01Application.java
@@ -0,0 +1,13 @@
+package aDarbellay.s05.t1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S05T01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S05T01Application.class, args);
+	}
+
+}
