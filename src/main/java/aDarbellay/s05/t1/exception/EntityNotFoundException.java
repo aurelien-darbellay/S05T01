7da7e61dd29ba14693beabb4705ed9515e82ace0
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-05-18 20:46:42 +0200

    insignificant change to try pushing

--- src/main/java/aDarbellay/s05/t1/exception/EntityNotFoundException.java
+++ src/main/java/aDarbellay/s05/t1/exception/EntityNotFoundException.java
@@ -3,6 +3,6 @@ package aDarbellay.s05.t1.exception;
 public class EntityNotFoundException extends Exception {
 
     public EntityNotFoundException(Class<?> clazz, String id) {
-        super(String.format("Error: entity of type %s with identifier %s not found.", clazz.getSimpleName(), id));
+        super(String.format("Error: entity of type %s with id %s not found.", clazz.getSimpleName(), id));
     }
 }
